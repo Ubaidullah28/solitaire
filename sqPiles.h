cd477234d1e57008701b778076ee8150f9a956c4
Ubaidullah28 <130082952+Ubaidullah28@users.noreply.github.com>
2023-10-01 19:33:22 +0500

    Update sqPiles.h

--- sqPiles.h
+++ sqPiles.h
@@ -1,1 +1,45 @@
 #pragma once
+#include"utility.h"
+#include"pile.h"
+
+
+class sqPile : virtual public pile
+{
+public:
+	sqPile(sf::Vector2i = sf::Vector2i(0, 0))
+	{
+
+	}
+
+	void draw(sf::RenderWindow & win) 
+	{
+		if (getSize() == 0)
+		{
+				drawEmpty(win);
+		}
+		else
+		{
+				cards.back().draw(win);
+		}
+	}
+
+
+	size_t getWidth() const 
+	{
+		return pileofCard::width;
+	}
+
+	size_t getHeight() const 
+	{
+		return pileofCard::height;
+	}
+	void resetPos();
+	int getCardAt(sf::Vector2i point);
+
+	sf::Vector2i getCardPosition(size_t)
+	{
+		return offset;
+	}
+
+};
+
