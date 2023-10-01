c07f463f3be7043e522f26cbb119bdf0aafebe6e
Ubaidullah28 <130082952+Ubaidullah28@users.noreply.github.com>
2023-10-01 19:35:30 +0500

    Update stock.h

--- stock.h
+++ stock.h
@@ -1,1 +1,24 @@
 #pragma once
+#include"sqPiles.h"
+#include"pile.h"
+class borrowPile;
+
+class stockPile : public sqPile
+{
+	borrowPile* bPile;
+public:
+	stockPile(borrowPile* _bPile, sf::Vector2i _offset = sf::Vector2i(0, 0))
+		: pile(_offset), sqPile(_offset), bPile(_bPile)
+	{
+	}
+	bool isSplit(size_t s)
+	{
+		return false;
+	}
+
+	bool isAppend(const pile&p)
+	{
+		return false;
+	}
+	
+};
