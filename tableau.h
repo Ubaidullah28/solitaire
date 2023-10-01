80204addc7dcf494176dab4961ff2ff27509ca22
Ubaidullah28 <130082952+Ubaidullah28@users.noreply.github.com>
2023-10-01 19:37:16 +0500

    Update tableau.h

--- tableau.h
+++ tableau.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include"pile.h"
+
+class tableau 
+{
+public:
+	tableau(sf::Vector2i offset = sf::Vector2i(0, 0), sf::Vector2f visibility = sf::Vector2f(28.0, 14.0))
+		
+	{
+
+	}
+
+	bool canSplit(unsigned int);
+	bool canAppend(const pile&);
+};
