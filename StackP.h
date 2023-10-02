407d88659c85ba0f010de3151c4aa2797b712216
MAbdullah369 <130146254+MAbdullah369@users.noreply.github.com>
2023-10-02 11:24:35 +0500

    Update StackP.h

--- StackP.h
+++ StackP.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "PileofCards.h"
+#include "pile.h"
+
+
+class stackPile : virtual public pile
+{
+public:
+	stackPile(sf::Vector2i = sf::Vector2i(0, 0));
+	stackPile(sf::Vector2i offset) :pile(offset) {
+	}
+
+	bool canSplit(unsigned int cardIndex) {
+		return commonSplitTest(cardIndex) && cardIndex == getSize() - 1;
+	}
+
+	~stackPile(void) {
+	}
+};
+
